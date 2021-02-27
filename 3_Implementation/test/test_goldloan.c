99e908cff47430ae1680b1c270ca407ca20127a3
99003573 <78539753+99003573@users.noreply.github.com>
2021-02-27 15:35:43 +0530

    Update test_goldloan.c

--- 3_Implementation/test/test_goldloan.c
+++ 3_Implementation/test/test_goldloan.c
@@ -8,14 +8,9 @@ void test_Goldloan_basic(void)
 	TEST_ASSERT_EQUAL(584,Goldloan(5) );
 }
 
-int test_Goldloan(void)
+void test_Goldloan(void)
 {
-	/* Initiate the Unity Test Framework */
-	UNITY_BEGIN();
 
 	/* Run Test functions */
 	RUN_TEST(test_Goldloan_basic);
-
-	/* Close the Unity Test Framework */
-	return UNITY_END();
 }
